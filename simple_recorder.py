02ee294598544e32d35e4704e03897887d6f000a
Nick Fox-Gieg <nick@fox-gieg.com>
2024-04-07 22:25:38 -0400

    Update simple_recorder.py

--- simple_recorder.py
+++ simple_recorder.py
@@ -4,8 +4,20 @@ import time
 resolution = (1920, 1080)
 framerate = 24
 
-# Initialize the camera
 camera = picamera.PiCamera(resolution=resolution, framerate=framerate)
+
+# https://picamera.readthedocs.io/en/release-1.13/recipes1.html
+
+# Set ISO to the desired value
+camera.iso = 400
+# Wait for the automatic gain control to settle
+sleep(2)
+# Now fix the values
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
 
 try:
     while True:
